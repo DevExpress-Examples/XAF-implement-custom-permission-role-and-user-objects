a546ce815794be4d0a1ad8aec4e34b14b2ef6159
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-10 17:49:33 +0400

    add xpo

---
+++ CS/XPO/MySolutionXPO/MySolutionXPO.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using MySolutionXPO.Blazor.Server.Services;
+
+namespace MySolutionXPO.Blazor.Server;
+
+public class MySolutionXPOBlazorApplication : BlazorApplication {
+    public MySolutionXPOBlazorApplication() {
+        ApplicationName = "MySolutionXPO";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += MySolutionXPOBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void MySolutionXPOBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
---
+++ CS/XPO/MySolutionXPO/MySolutionXPO.Win/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Win.ApplicationBuilder;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.XtraEditors;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.ExpressApp.Design;
+
+namespace MySolutionXPO.Win;
+
+public class ApplicationBuilder : IDesignTimeApplicationFactory {
+    public static WinApplication BuildApplication(string connectionString) {
+        var builder = WinApplication.CreateBuilder();
+        builder.UseApplication<MySolutionXPOWindowsFormsApplication>();
+        builder.Modules
+            .AddConditionalAppearance()
+            .AddReports(options => {
+                options.EnableInplaceReports = true;
+                options.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+                options.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+            })
+            .AddValidation(options => {
+                options.AllowValidationDetailsAccess = false;
+            })
+            .Add<MySolutionXPO.Module.MySolutionXPOModule>()
+        	.Add<MySolutionXPOWinModule>();
+        builder.ObjectSpaceProviders
+            .AddSecuredXpo((application, options) => {
+                options.ConnectionString = connectionString;
+            })
+            .AddNonPersistent();
+        builder.Security
+            .UseIntegratedMode(options => {
+                options.RoleType = typeof(PermissionPolicyRole);
+                options.UserType = typeof(MySolutionXPO.Module.BusinessObjects.ApplicationUser);
+                options.UserLoginInfoType = typeof(MySolutionXPO.Module.BusinessObjects.ApplicationUserLoginInfo);
+                options.UseXpoPermissionsCaching();
+            })
+            .UsePasswordAuthentication();
+        builder.AddBuildStep(application => {
+            application.ConnectionString = connectionString;
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached && application.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                application.DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            }
+#endif
+        });
+        var winApplication = builder.Build();
+        return winApplication;
+    }
+
+    XafApplication IDesignTimeApplicationFactory.Create()
+        => BuildApplication(XafApplication.DesignTimeConnectionString);
+}
