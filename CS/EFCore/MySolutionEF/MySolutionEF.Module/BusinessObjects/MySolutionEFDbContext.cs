dc11eb0ea3e288d1bee48ea39bc23f9ec5c38f9b
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-10 18:43:11 +0400

    add efcore

---
+++ CS/EFCore/MySolutionEF/MySolutionEF.Module/BusinessObjects/MySolutionEFDbContext.cs
@@ -0,0 +1,56 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+
+namespace MySolutionEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class MySolutionEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<MySolutionEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new MySolutionEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class MySolutionEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<MySolutionEFEFCoreDbContext> {
+	public MySolutionEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<MySolutionEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=MySolutionEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new MySolutionEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(MySolutionEFContextInitializer))]
+public class MySolutionEFEFCoreDbContext : DbContext {
+	public MySolutionEFEFCoreDbContext(DbContextOptions<MySolutionEFEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ModelDifference> ModelDifferences { get; set; }
+	public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	public DbSet<PermissionPolicyRole> Roles { get; set; }
+	public DbSet<MySolutionEF.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<MySolutionEF.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+	public DbSet<ReportDataV2> ReportDataV2 { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<MySolutionEF.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+        modelBuilder.Entity<ModelDifference>()
+            .HasMany(t => t.Aspects)
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ CS/EFCore/MySolutionEF/MySolutionEF.Win/WinApplication.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Win.Utils;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using Microsoft.EntityFrameworkCore;
+using DevExpress.ExpressApp.EFCore;
+using DevExpress.EntityFrameworkCore.Security;
+using MySolutionEF.Module;
+using MySolutionEF.Module.BusinessObjects;
+using System.Data.Common;
+
+namespace MySolutionEF.Win;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Win.WinApplication._members
+public class MySolutionEFWindowsFormsApplication : WinApplication {
+    public MySolutionEFWindowsFormsApplication() {
+		SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
+        ApplicationName = "MySolutionEF";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        UseOldTemplates = false;
+        DatabaseVersionMismatch += MySolutionEFWindowsFormsApplication_DatabaseVersionMismatch;
+        CustomizeLanguagesList += MySolutionEFWindowsFormsApplication_CustomizeLanguagesList;
+    }
+    private void MySolutionEFWindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e) {
+        string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+        if(userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1) {
+            e.Languages.Add(userLanguageName);
+        }
+    }
+    private void MySolutionEFWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+			string message = "The application cannot connect to the specified database, " +
+				"because the database doesn't exist, its version is older " +
+				"than that of the application or its schema does not match " +
+				"the ORM data model structure. To avoid this error, use one " +
+				"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+			if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+				message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+			}
+			throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
